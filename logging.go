6c34aecf925d1a34ed92f13b1e93453c2ab782c3
AnimeshRy <ani16september@gmail.com>
2024-11-05 00:06:18 +0530

    feat: logging setup

---
+++ logging.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type loggingService struct {
+	next PriceFetcher
+}
+
+func (s *loggingService) FetchPrice(ctx context.Context, ticker string) (price float64, err error) {
+	defer func(begin time.Time) {
+		logrus.WithFields(logrus.Fields{
+			"took":  time.Since(begin),
+			"err":   err,
+			"price": price,
+		}).Info("fetchPrice")
+	}(time.Now())
+
+	return s.next.FetchPrice(ctx, ticker)
+}
--- main.go
+++ main.go
@@ -1,50 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-type Message struct {
-	From    string
-	Payload string
-}
-
-type Server struct {
-	msgch chan Message
-}
-
-func (s *Server) StartAndListen() {
-	for {
-		select {
-		// block here until someone is sending a messag to the channel
-		case msg := <-s.msgch:
-			fmt.Printf("Received message from %s: %s\n", msg.From, msg.Payload)
-		default:
-		}
-	}
-}
-
-func sendMessageToServer(msgch chan Message, payload string) {
-	msg := Message{
-		From:    "server",
-		Payload: payload,
-	}
-	msgch <- msg
-}
-
 func main() {
-	s := &Server{
-		msgch: make(chan Message),
-	}
-	go s.StartAndListen()
-	var count = 4
-	for i := 0; i < count; i++ {
-		go func(i int) {
-			time.Sleep(time.Duration(i) * 2 * time.Second)
-			sendMessageToServer(s.msgch, fmt.Sprintf("Hello, world %d!", i+1))
-		}(i)
-	}
 
-	select {}
 }
