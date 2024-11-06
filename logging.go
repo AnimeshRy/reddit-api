a0e9964ddf85bea10d83c954be6ca025c5a64bfb
AnimeshRy <ani16september@gmail.com>
2024-11-06 20:16:00 +0530

    this should suffice as enough boilerplate, now I gotta sleepgit add .

--- logging.go
+++ logging.go
@@ -11,12 +11,19 @@ type loggingService struct {
 	next PriceFetcher
 }
 
+func NewLoggingService(next PriceFetcher) PriceFetcher {
+	return &loggingService{
+		next: next,
+	}
+}
+
 func (s *loggingService) FetchPrice(ctx context.Context, ticker string) (price float64, err error) {
 	defer func(begin time.Time) {
 		logrus.WithFields(logrus.Fields{
-			"took":  time.Since(begin),
-			"err":   err,
-			"price": price,
+			"requestID": ctx.Value("requestID"),
+			"took":      time.Since(begin),
+			"err":       err,
+			"price":     price,
 		}).Info("fetchPrice")
 	}(time.Now())
 
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+)
 
+func main() {
+	svc := NewLoggingService(NewMetricService(&priceFetcher{}))
+
+	price, err := svc.FetchPrice(context.Background(), "ETH")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(price)
 }
